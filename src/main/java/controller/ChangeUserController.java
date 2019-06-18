1e5187603b667f17752a10d0a31367eea41a8b65
Danielle van den Bos <danielle@vdbos.nl>
2019-06-18 16:27:40 +0200

    UserDAO pull nodig

--- src/main/java/controller/ChangeUserController.java
+++ src/main/java/controller/ChangeUserController.java
@@ -1,13 +1,43 @@
 package controller;
 
 import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import model.Database.UserDAO;
 import model.entity.User.User;
+import view.Main;
+
+
 
 public class ChangeUserController {
 
-  public void setup(User user) {}
+  @FXML
+  private TextField nameField;
 
-  public void doMenu(ActionEvent event){}
+  @FXML
+  private TextField passwordField;
 
-  public void doChangeUser(ActionEvent event){}
+  public void setup(User user) {
+  }
+
+  public void doMenu(ActionEvent event) {
+    Main.getSceneManager().showLoginScene();
+  }
+
+  public void changeRole(String name) {
+
+  }
+
+public void doChangeUser(ActionEvent event){
+    String name = nameField.getText();
+    String passwoord = passwordField.getText();
+
+    UserDAO udao = UserDAO.getInstance();
+    }
+
+    //update sql
+  }
+  }
 }
