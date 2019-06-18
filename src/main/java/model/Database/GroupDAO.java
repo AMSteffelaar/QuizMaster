e318740d4ab01560d52b22e4d8be745dc215e50d
Tristan <tristanwesenhagen@gmail.com>
2019-06-18 16:36:56 +0200

    UserDAO nodig

--- src/main/java/model/Database/GroupDAO.java
+++ src/main/java/model/Database/GroupDAO.java
@@ -1,22 +1,21 @@
 package model.Database;
 
-import model.entity.User.User;
 import view.Main;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserDAO extends AbstractDAO {
-    private static UserDAO udao;
+public class GroupDAO extends AbstractDAO{
+    private static GroupDAO gdao;
 
-    public UserDAO(DBaccess db) {
+    public GroupDAO(DBaccess db) {
         super(db);
     }
 
     public int getUserIdByNamePassword(String name, String password) {
         String sql = "Select * from user where name = ? and password = ?";
-        int idUser = 0;
+        int id = 0;
         try {
             PreparedStatement ps = getStatement(sql);
             ps.setString(1, name);
@@ -24,25 +23,22 @@ public class UserDAO extends AbstractDAO {
             ResultSet rs = executeSelectPreparedStatement(ps);
 
             while (rs.next()) {
-                idUser = rs.getInt("idUser");
+                id = rs.getInt("idUser");
 
             }
         } catch (SQLException e) {
             System.out.println("SQL error " + e.getMessage());
         }
-        return idUser;
+        return id;
     }
 
-
-    public static UserDAO getInstance(){
-        if(udao==null){
-            udao = new UserDAO(Main.getInstance());
-            return udao;
+    public static GroupDAO getInstance(){
+        if(gdao==null){
+            gdao = new GroupDAO(Main.getInstance());
+            return gdao;
         }
         else {
-            return udao;
+            return gdao;
         }
     }
 }
-
-
