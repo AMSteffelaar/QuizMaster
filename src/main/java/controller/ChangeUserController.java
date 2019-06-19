658a5de21a401094eb822a11c41c774f562248fe
Danielle van den Bos <danielle@vdbos.nl>
2019-06-19 13:43:34 +0200

    Change user dropdown begin

--- src/main/java/controller/ChangeUserController.java
+++ src/main/java/controller/ChangeUserController.java
@@ -4,9 +4,7 @@ import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Database.UserDAO;
@@ -14,43 +12,50 @@ import model.entity.User.User;
 import view.Main;
 import view.SceneManager;
 
+import java.util.ArrayList;
+
 
 public class ChangeUserController {
 
+    @FXML
+    public MenuButton roleMenuButton;
+    ArrayList<String> menuRoles = new ArrayList<>();
     // In de selectUserContoller moet nog gemaakt worden hier wordt dan een gebruiker geselecteerd.
     // Deze gebruiker moet dan gewijzigd worden. DIT IS DUS NOG NIET AF!
     @FXML
     private TextField nameField;
-
     @FXML
     private TextField passwordField;
 
-    Stage window;
-    Button button;
-    ComboBox<String> comboBox;
-
     public void setup(User user) {
+        populateRoleMenu();
     }
 
     public void doMenu(ActionEvent event) {
         Main.getSceneManager().showLoginScene();
     }
 
+
     public void changeRole(ActionEvent event) throws Exception {
-        comboBox = new ComboBox<>();
-        comboBox.getItems().addAll("Student", "Docent", "Coordinator");
 
-        VBox layout = new VBox(10);
-        layout.setPadding(new Insets(20,20,20,20));
-        layout.getChildren().addAll(comboBox);
+    }
 
+    public void populateRoleMenu(){
+        menuRoles.add("Student");
+        menuRoles.add("Docent");
+        for (String role : menuRoles) {
+            {
+                MenuItem item = new MenuItem();
+                item.setText(role);
+                roleMenuButton.getItems().add(item);
+            }
+        }
     }
 
     public void doChangeUser(ActionEvent event) {
         String name = nameField.getText();
         String password = passwordField.getText();
         UserDAO udao = UserDAO.getInstance();
-
         //deze is wel anders
         int iduser = udao.getUserIdByNamePassword(name, password);
         User user = udao.changeUser(name, password);
@@ -59,8 +64,5 @@ public class ChangeUserController {
         } else {
             SceneManager.getSceneManager().showLoginFailedScene();
         }
-    }
-
-    public static void main(String[] args) {
     }
 }
