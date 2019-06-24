a2d1e332038e2136c599e286deb771bc634220de
Joost Kager <joost.kager@hva.nl>
2019-06-25 00:55:58 +0200

    controller StudentSignIn/Out in basis operationeel

--- src/main/java/controller/StudentSignInOutController.java
+++ src/main/java/controller/StudentSignInOutController.java
@@ -6,9 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import model.Database.CourseDAO;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.paint.Paint;
 import model.Database.GroupDAO;
-import model.entity.Course;
 import model.entity.Group;
 import model.entity.Session;
 import view.SceneManager;
@@ -16,77 +16,94 @@ import view.SceneManager;
 import java.util.ArrayList;
 
 public class StudentSignInOutController {
-  SceneManager manager = SceneManager.getSceneManager();
-  CourseDAO cdao = CourseDAO.getInstance();
-  GroupDAO gdao = GroupDAO.getInstance();
+    SceneManager manager = SceneManager.getSceneManager();
+    GroupDAO gdao = GroupDAO.getInstance();
 
-  private int studentId = Session.getInstance().getCurrentUser().getId();
-  ArrayList<Group> ingeschreven_groepen = gdao.studentInGroups(studentId);
+    private int studentId = Session.getInstance().getCurrentUser().getId();
+    ArrayList<Group> ingeschreven_groepen = gdao.studentInGroups(studentId);
 
-  @FXML
-  private Button signInButton;
+    @FXML
+    private Button signInButton;
 
-  @FXML
-  private Button signOutButton;
+    @FXML
+    private Button signOutButton;
 
-  @FXML
-  private ListView courseList = new ListView(); //is de courselist not signed in
+    @FXML
+    private ListView<Group> courseList = new ListView(); //is de courselist not signed in
 
-  @FXML
-  private ListView SignedInCourseList = new ListView();
+    @FXML
+    private ListView<Group> SignedInCourseList = new ListView();
 
 
-  public void setup() {
-    populateCourselist();
-    populateCourseList_not_signed_in();
-  }
-
-  public void doMenu(ActionEvent event){
-    manager.showWelcomeScene();
-  }
-
-  public void doSignIn(ActionEvent event){
-    System.out.println(courseList.getSelectionModel().getSelectedItems());
-  }
-
-  public void doSignOut(ActionEvent event){
-    System.out.println(SignedInCourseList.getSelectionModel().getSelectedItems());
-  }
-
-  private void populateCourselist(){ // mag ook heten signedIn courses
-    ObservableList<String> cursus = FXCollections.observableArrayList();
-    ;//courses moet zijn groups
-    for (Group group : ingeschreven_groepen) {
-      cursus.add(group.getCourse().getName());
+    public void setup() {
+        signInButton.setTextFill(Paint.valueOf("DarkGreen")); //bepaalt kleur van tekst
+        signInButton.setStyle("-fx-background-color: Green");//bepaalt kleur van inschrijfknop
+        signOutButton.setTextFill(Paint.valueOf("Black"));//bepaalt kleur tekst uitschrijfknop
+        signOutButton.setStyle("-fx-background-color: Red");//bepaalt kleur uitschrijfknop
+        populateCourselist();
+        courseList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        populateCourseList_not_signed_in();
+        SignedInCourseList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
-    SignedInCourseList.setItems(cursus);
-  }
 
-  private void populateCourseList_not_signed_in (){
-    ObservableList<String> cursus = FXCollections.observableArrayList();
-    ArrayList<Group> groups = gdao.getGroups();
-    // met een loop in een loop nagaan in welke groep de student niet zit,
-    // van zo'n groep de bijbehorende cursus naam in een arrayList met cursussen zetten
-    ArrayList<Group> groups_not_signed_in = new ArrayList<>();
-    for (Group g : groups ) {
-      // kijk of g voorkomt in de lijst van de student
-      // maak een boolean komt voor, zet die op false
-      boolean komt_voor = false;
-      for (Group g_this_student : ingeschreven_groepen) {
-        if (g.getIdGroup() == g_this_student.getIdGroup()) {
-          komt_voor = true;
+    public void doMenu(ActionEvent event) {
+        manager.showWelcomeScene();
+    }
+
+    public void doSignIn(ActionEvent event) {
+        ObservableList<Group> keuzeInschrijven = courseList.getSelectionModel().getSelectedItems();
+        for (Group g : keuzeInschrijven) {
+            gdao.enrollStudentInGroup(g, studentId);
+            manager.showStudentSignInOutScene();
         }
-      }
-      // als komt_voor nog steeds false dan g op de lijst zetten
-      if (komt_voor == false) {
-        groups_not_signed_in.add(g);
-      } //door naar volgende
     }
-    for (Group g : groups_not_signed_in) {
-      cursus.add(g.getCourse().getName());
-    }
-    courseList.setItems(cursus);
-  }
 
+    public void doSignOut(ActionEvent event) {
+        ObservableList<Group> keuzeUitschrijven = SignedInCourseList.getSelectionModel().getSelectedItems();
+        for (Group g : keuzeUitschrijven) {
+            gdao.withdrawStudentFromGroup(g, studentId);
+            populateCourseList_not_signed_in();
+            manager.showStudentSignInOutScene();
+        }
+    }
+
+    /**
+     * Helper methode om linker listview te vullen met Cursussen waarvoor de student die is ingelogd zich heeft ingeschreven.
+     * dit is te herleiden door een notering in de koppeltabel studentsingroup, waarna op basis van groupid de bijpassende course kan worden getoond
+     */
+    private void populateCourselist() { // mag ook heten signedIn courses
+        ObservableList<Group> groepen = FXCollections.observableArrayList();
+        groepen.setAll(ingeschreven_groepen);
+        SignedInCourseList.setItems(groepen);
+    }
+
+    /**
+     * Helper methode om een lijst te creëren met vermelding van alle groepen (en daarmee ook cursussen) waarvoor de student zich nog niet heeft ingeschreven.
+     */
+    private void populateCourseList_not_signed_in() {
+        ObservableList<Group> cursus = FXCollections.observableArrayList();
+        ArrayList<Group> groups = gdao.getGroups();
+        // met een loop in een loop nagaan in welke groep de student niet zit,
+        // en deze toevoegen in de lijst cursus
+        ArrayList<Group> groups_not_signed_in = new ArrayList<>();
+        for (Group g : groups) {
+            // kijk of g voorkomt in de lijst van de student
+            // maak een boolean komt voor, zet die op false
+            boolean komt_voor = false;
+            for (Group g_this_student : ingeschreven_groepen) {
+                if (g.getIdGroup() == g_this_student.getIdGroup()) {
+                    komt_voor = true;
+                }
+            }
+            // als komt_voor nog steeds false dan g op de lijst zetten
+            if (komt_voor == false) {
+                groups_not_signed_in.add(g);
+            } //door naar volgende
+        }
+        for (Group g : groups_not_signed_in) {
+            cursus.add(g);
+        }
+        courseList.setItems(cursus);
+    }
 }
 
