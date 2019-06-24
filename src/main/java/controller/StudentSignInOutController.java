30ae96f36bcb91637373597e5a103d4e56121665
Joost Kager <joost.kager@hva.nl>
2019-06-24 14:30:31 +0200

    kleine sout ter controlle op knoppen signin /signout

--- src/main/java/controller/StudentSignInOutController.java
+++ src/main/java/controller/StudentSignInOutController.java
@@ -43,9 +43,13 @@ public class StudentSignInOutController {
     manager.showWelcomeScene();
   }
 
-  public void doSignIn(ActionEvent event){}
+  public void doSignIn(ActionEvent event){
+    System.out.println(courseList.getSelectionModel().getSelectedItems());
+  }
 
-  public void doSignOut(ActionEvent event){}
+  public void doSignOut(ActionEvent event){
+    System.out.println(SignedInCourseList.getSelectionModel().getSelectedItems());
+  }
 
   private void populateCourselist(){
     ObservableList<String> cursus = FXCollections.observableArrayList();
