c796f6aab4fe7e4757d0eb7560c692de5a9279e6
Tristan <tristanwesenhagen@gmail.com>
2019-06-24 14:37:39 +0200

    QuestionDAO alvast pushen voor Danielle

--- src/main/java/model/Database/QuestionDAO.java
+++ src/main/java/model/Database/QuestionDAO.java
@@ -1,21 +1,56 @@
 package model.Database;
 
+import model.entity.Course;
+import model.entity.Group;
 import model.entity.Question;
+import model.entity.Quiz;
+import model.entity.User.User;
 import view.Main;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class QuestionDAO extends AbstractDAO {
 
-    public static QuestionDAO getInstance(){
-        if(quesdao==null){
-            quesdao = new QuestionDAO(Main.getInstance());
-            return quesdao;
+    private static QuestionDAO questdao;
+
+    public QuestionDAO(DBaccess db) {super(db);}
+
+        public static QuestionDAO getInstance(){
+        if(questdao==null){
+            questdao = new QuestionDAO(Main.getInstance());
+            return questdao;
         }
         else {
-            return quesdao;
+            return questdao;
         }
     }
 
-
+    public ArrayList<Question> getQuestionbyQuiz(int idQuiz) {
+        String sql = "SELECT * FROM q&a where Quiz_idQuiz = ?";
+        ArrayList<Question> results = null;
+        Question result;
+        try {
+            PreparedStatement ps = getStatement(sql);
+            ps.setInt(1, idQuiz);
+            ResultSet rs = executeSelectPreparedStatement(ps);
+            results = new ArrayList<>();
+            while (rs.next()) {
+                int quiz_idQuiz = rs.getInt("Quiz_idQuiz");
+                String question = rs.getString("question");
+                QuizDAO qdao = QuizDAO.getInstance();
+                Quiz quiz = qdao.getQuizById(quiz_idQuiz);
+                result = new Question(quiz, question);
+                result.setIdQuestion(questionId);
+                results.add(result);
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL error " + e.getMessage());
+        }
+        return results;
+    }
 
 
 }
